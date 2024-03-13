71d0f8dbeb8f269ac3c3ee9feffa50ed19c3e726
akshay@96804 <19uec032@lnmiit.ac.in>
2024-03-14 00:40:39 +0530

    HashRouter updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header';
 import Home from './pages/home/home';
 import MovieList from './components/movieList/movieList';
@@ -8,7 +8,7 @@ import Movie from './pages/movieDetail/movie';
 function App() {
   return (
     <div className="App">
-        <Router>
+        <HashRouter>
           <Header />
             <Routes>
                 <Route index element={<Home />}></Route>
@@ -16,7 +16,7 @@ function App() {
                 <Route path="movies/:type" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
-        </Router>
+        </HashRouter>
     </div>
   );
 }
